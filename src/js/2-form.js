baa4dc7e5fae55f3f244f38eea5ac24f91cb5344
Serhiy Malysh <166062683+finconsultsm@users.noreply.github.com>
2025-11-24 17:59:01 +0100

    final commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,56 @@
-console.log("Form");
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+
+let formData = {
+    email: "",
+    message: "",
+}
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+    try {
+        const parsedData = JSON.parse(savedData);
+
+        formData.email = parsedData.email || "";
+        formData.message = parsedData.message || "";
+
+        form.elements.email.value = formData.email;
+        form.elements.message.value = formData.message;
+    } catch (error) {
+        console.log("Invalid data in localStorage, clearing:", error);
+        localStorage.removeItem(STORAGE_KEY);
+    }
+}
+
+form.addEventListener("input", handleInput);
+
+function handleInput(event) {
+    const field = event.target.name;
+    if (!(field in formData)) return;
+    const value = event.target.value.trim();
+
+    formData[field] = value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert('Заповніть дві форми');
+        return;
+    }
+
+    console.log(formData);
+    
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData = {
+        email: "",
+        message: "",
+    }
+}
